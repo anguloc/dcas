9477ad165c47948281f5095825bf700c627f90e4
gk <xy62201793@163.com>
2021-04-08 23:15:18 +0800

    log

--- utils/log/log.go
+++ utils/log/log.go
@@ -14,7 +14,14 @@ func init() {
 	if config.Conf.Log.FilePath == "" {
 		return
 	}
-	logger = getLogger(config.Conf.Log.FilePath, zapcore.DebugLevel, 1, 30, 7, true, "Main")
+	logger = getLogger(config.Conf.Log.FilePath, zapcore.DebugLevel, 4, 3, 3, true)
+}
+
+func Debug(msg string, args ...interface{}) {
+	if logger == nil {
+		return
+	}
+	logger.Debug(msg)
 }
 
 func Info(msg string, args ...interface{}) {
@@ -22,6 +29,27 @@ func Info(msg string, args ...interface{}) {
 		return
 	}
 	logger.Info(msg)
+}
+
+func Error(msg string, args ...interface{}) {
+	if logger == nil {
+		return
+	}
+	logger.Error(msg)
+}
+
+func Panic(msg string, args ...interface{}) {
+	if logger == nil {
+		return
+	}
+	logger.Panic(msg)
+}
+
+func Fatal(msg string, args ...interface{}) {
+	if logger == nil {
+		return
+	}
+	logger.Fatal(msg)
 }
 
 /**
@@ -32,9 +60,8 @@ func Info(msg string, args ...interface{}) {
  * maxBackups 日志文件最多保存多少个备份
  * maxAge 文件最多保存多少天
  * compress 是否压缩
- * serviceName 服务名
  */
-func getLogger(filePath string, level zapcore.Level, maxSize int, maxBackups int, maxAge int, compress bool, serviceName string) *zap.Logger {
+func getLogger(filePath string, level zapcore.Level, maxSize int, maxBackups int, maxAge int, compress bool) *zap.Logger {
 	hook := lumberjack.Logger{
 		Filename:   filePath,   // 日志文件路径
 		MaxSize:    maxSize,    // 每个日志文件保存的最大尺寸 单位：M
@@ -42,27 +69,16 @@ func getLogger(filePath string, level zapcore.Level, maxSize int, maxBackups int
 		MaxAge:     maxAge,     // 文件最多保存多少天
 		Compress:   compress,   // 是否压缩
 	}
-	// 设置日志级别
+
 	atomicLevel := zap.NewAtomicLevel()
 	atomicLevel.SetLevel(level)
-	//公用编码器
-	encoderConfig := zapcore.EncoderConfig{
-		TimeKey:        "time",
-		LevelKey:       "level",
-		NameKey:        "logger",
-		CallerKey:      "linenum",
-		MessageKey:     "msg",
-		StacktraceKey:  "stacktrace",
-		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
-		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
-		EncodeDuration: zapcore.SecondsDurationEncoder, //
-		EncodeCaller:   zapcore.FullCallerEncoder,      // 全路径编码器
-		EncodeName:     zapcore.FullNameEncoder,
-	}
 
-	// 编码器配置
-	encoder := zapcore.NewJSONEncoder(encoderConfig)
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder // 时间格式换下
+	encoderConfig.EncodeCaller = zapcore.FullCallerEncoder // 调用路径从根目录起
+
+	encoder := zapcore.NewJSONEncoder(encoderConfig) // json 格式
+	//encoder := zapcore.NewConsoleEncoder(encoderConfig) // 单行格式
 
 	// 输出
 	writeSyncer := zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook))
@@ -71,12 +87,10 @@ func getLogger(filePath string, level zapcore.Level, maxSize int, maxBackups int
 
 	// 开启开发模式，堆栈跟踪
 	caller := zap.AddCaller()
-	// 开启文件及行号
-	development := zap.Development()
-	// 设置初始化字段
-	filed := zap.Fields(zap.String("type", "default"))
+	// 开启文件及行号 跳过封装层
+	development := zap.AddCallerSkip(1)
 	// 构造日志
-	logger := zap.New(core, caller, development, filed)
+	logger := zap.New(core, caller, development)
 
 	return logger
 }
